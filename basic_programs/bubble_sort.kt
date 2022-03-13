6035d27d95ba45d4f08766dc23d9de0580c19797
Jeevan Philip <jgphilip@gmail.com>
2022-03-13 01:36:06 -0500

    added code for bubble sort

---
+++ basic_programs/bubble_sort.kt
@@ -0,0 +1,21 @@
+fun main(){
+	val unsortedList = intArrayOf(0,23,44,22,1,4445)
+	val sortedList = bubbleSort(unsortedList)
+	for ( value in sortedList) print("$value ")
+}
+
+fun bubbleSort(arr : IntArray) : IntArray {
+	var swap = true
+	while(swap){
+		swap = false
+		for(i in 0 until arr.size-1){
+		    if(arr[i] > arr[i+1]){
+			 val temp = arr[i]
+			 arr[i] = arr[i+1]
+			 arr[i+1] = temp
+			swap = true
+		    }		
+		}
+	}
+	return arr
+}
